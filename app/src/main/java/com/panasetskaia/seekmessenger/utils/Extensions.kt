500f4ca52e38220f461cd7490540f8e526ace51d
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-12-11 18:58:16 +0800

    add extensions for FirebaseAuth

---
+++ app/src/main/java/com/panasetskaia/seekmessenger/utils/Extensions.kt
@@ -0,0 +1,54 @@
+package com.panasetskaia.seekmessenger.utils
+
+import android.app.Activity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.panasetskaia.seekmessenger.R
+
+fun FirebaseAuth.createUser(
+    email: String,
+    password: String,
+    context: Activity,
+    updateUI: (FirebaseUser?) -> Unit
+) {
+    this.createUserWithEmailAndPassword(email, password).addOnCompleteListener(context) { task ->
+        if (task.isSuccessful) {
+            Log.d("Firebase", "createUserWithEmail:success")
+            val user = this.currentUser
+            updateUI(user)
+        } else {
+            Log.w("Firebase", "createUserWithEmail:failure", task.exception)
+            Toast.makeText(
+                context, context.getString(R.string.auth_failed),
+                Toast.LENGTH_SHORT
+            ).show()
+            updateUI(null)
+        }
+    }
+
+}
+
+fun FirebaseAuth.signInUser(
+    email: String,
+    password: String,
+    context: Activity,
+    updateUI: (FirebaseUser?) -> Unit
+) {
+    this.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(context) { task ->
+            if (task.isSuccessful) {
+                Log.d("Firebase", "signInWithEmail:success")
+                val user = this.currentUser
+                updateUI(user)
+            } else {
+                Log.w("Firebase", "signInWithEmail:failure", task.exception)
+                Toast.makeText(
+                    context, context.getString(R.string.auth_failed),
+                    Toast.LENGTH_SHORT
+                ).show()
+                updateUI(null)
+            }
+        }
+}
