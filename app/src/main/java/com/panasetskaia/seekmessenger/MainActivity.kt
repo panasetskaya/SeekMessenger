1307b58051002add9e962ce85306b2f21e6d1052
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-12-11 18:44:34 +0800

    add firebase

--- app/src/main/java/com/panasetskaia/seekmessenger/MainActivity.kt
+++ app/src/main/java/com/panasetskaia/seekmessenger/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.panasetskaia.seekmessenger
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser: FirebaseUser? = auth.currentUser
+        if (currentUser != null) {
+            goToChats()
+        } else {
+            goToLogin()
+        }
+    }
+
+    private fun goToLogin() {
+
+    }
+
+    private fun goToChats() {
+
     }
 }
